fb8f102942b1e213248d8024a339056566f23f7c
Paul Strong <lapluviosilla@gmail.com>
2024-10-21 14:38:56 -0600

    Add CSV Table Export, fixed some markdown preprocessing

--- plugin.config.js
+++ plugin.config.js
@@ -5,10 +5,10 @@
 export default {
   name: "Exporter",
   description: "A plugin to export your notes in other formats",
-  icon: "export_notes",
+  icon: "output",
   version: "1.0.0",
   sourceRepo: "https://github.com/acct/repo", // This is optional and can be removed
-  instructions: `![](https://linktoimage)
+  instructions: `
 Put any instructions **here**`,
-  setting: ["Setting #1 (default: false)", "Setting #2", "Setting #3"],
+  setting: [],
 };
